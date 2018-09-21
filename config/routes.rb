b3063abc4778cecf914d98582548e30ce0ddea59
johncburns1 <johncburns1@gmail.com>
2018-09-20 17:03:05 -0700

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/driver_guidelines'
-  get 'static_pages/washer_guidelines'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/driver_guidlines'
-  get 'static_pages/washer_guidlines'
-  get 'static_pages/DriverGuidlines'
-  get 'static_pages/WasherGuidlines'
-  resources :drivers
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/driver_guidelines', to: 'static_pages#driver_guidelines'
+  get '/washer_guidelines', to: 'static_pages#washer_guidelines'
+  get '/welcome', to: 'welcome_page#welcome'
 
+  resources :drivers
   resources :loads
   resources :admins
   resources :washers
 
-  get 'welcome_page/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome_page#welcome'
 end
