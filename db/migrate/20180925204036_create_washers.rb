8d4a0ff427cb72d7113d1fff2413d39b5fa821fa
johncburns1 <johncburns1@gmail.com>
2018-09-25 13:55:11 -0700

    add address fields to washer and driver

--- db/migrate/20180925204036_create_washers.rb
+++ db/migrate/20180925204036_create_washers.rb
@@ -5,16 +5,20 @@ class CreateWashers < ActiveRecord::Migration[5.2]
       t.string :password
       t.string :firstname
       t.string :lastname
-      t.string :phone
       t.string :email
-      t.string :address
+      t.string :phone
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.text :machine_description
       t.integer :washing_status
+      t.integer :confirmation_status
       t.integer :max_loads
       t.integer :current_loads
       t.integer :total_loads_completed
-      t.text :machine_description
       t.date :start_date
-      t.integer :confirmation_status
 
       t.timestamps
     end
