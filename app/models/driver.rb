decae7f1bced66d07e593938968612ea82dfaccb
loopiellcdev <loopiellcdev@gmail.com>
2018-09-25 13:57:41 -0700

    added some validation

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,14 @@
 class Driver < ApplicationRecord
   has_many :loads
-  
+
+  #validates if all required fields exist
+  validates_presence_of :username, :password
+
+  #validates if required unique fields are unique
+  validates_uniqueness_of :id, :username
+
+  #validation of lengths
+  validates_length_of :password
+
+
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class DriverTest < ActiveSupport::TestCase
+
+  def setup
+    @Driver = Driver.new(username: "ExUsername", password: "ExPassword")
+  end
+
+  test "Driver should exist" do
+    assert @Driver.valid?
+  end
+
+  test "Username should be present" do
+    @Driver.username = ""
+    assert_not @Driver.valid?
+  end
+
+  test "Password should be present" do
+    @Driver.password = ""
+    assert_not @Driver.valid?
+  end
+
+  
+
+
+
   # test "the truth" do
   #   assert true
   # end
