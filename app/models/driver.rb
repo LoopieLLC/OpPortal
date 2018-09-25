2f64a281d622165bc08ecef2dff25c3b5c7e31b6
loopiellcdev <loopiellcdev@gmail.com>
2018-09-25 14:37:47 -0700

    added more validation

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,14 +1,31 @@
 class Driver < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = ""
+
+
   has_many :loads
 
-  #validates if all required fields exist
-  validates_presence_of :username, :password
+#validates for username
+  validates :username,
+    presence: true,
+    length: { maximum: 50 },
+    uniqueness: true
 
-  #validates if required unique fields are unique
-  validates_uniqueness_of :id, :username
+#validates for password, needs 8 chars and 1 number, 1 special character
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
+    #format: { with: /(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#%&*]).{8,}/ },
+    #multiline: true
 
-  #validation of lengths
-  validates_length_of :password
+#validates email
+  validates :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 
+#validates 
 
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DriverTest < ActiveSupport::TestCase
 
   def setup
-    @Driver = Driver.new(username: "ExUsername", password: "ExPassword")
+    @Driver = Driver.new(username: "ExUsername", password: "ExPassword!23", email:"yeet@yeet.com")
   end
 
   test "Driver should exist" do
@@ -20,7 +20,13 @@ class DriverTest < ActiveSupport::TestCase
     assert_not @Driver.valid?
   end
 
-  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+
 
 
 
