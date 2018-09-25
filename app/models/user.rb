2b8399d3aebea26374938b455b974ec0defaae38
johncburns1 <johncburns1@gmail.com>
2018-09-25 15:25:53 -0700

    add default value

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   before_save { self.email = email.downcase }
+  before_save { self.confirmation_status = 0 }
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username,
     presence: true,
     length: { maximum: 50 },
-    uniqueness: { case_sensitive: false }
+    uniqueness: true
 
   validates :email,
     presence: true,
     length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+
+  validates :id,
+    presence: true,
+    uniqueness: true
 end
