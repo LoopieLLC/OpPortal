07be3c671f0edd9f3ccb15c3c4cc57c87f39b2ce
johncburns1 <johncburns1@gmail.com>
2018-10-01 11:39:57 -0700

    fix driver test

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -9,83 +9,45 @@ VALID_LP_REGEX = /\A[A-Z]{3}[0-9]{4}|[0-9]{3}-[A-Z]{3}\z/
 has_many :loads
 
 #validates for username
-  validates :username,
-    presence: true,
-    length: { maximum: 50 },
-    uniqueness: true
+validates :username,
+  presence: true,
+  length: { maximum: 50 },
+  uniqueness: true
 
 #validates for password, needs 8 chars and 1 number, 1 special character
-  validates :password,
-    presence: true,
-    length: { minimum: 8 },
-    format: { with: VALID_PASSWORD_REGEX }
+validates :password,
+  presence: true,
+  length: { minimum: 8 },
+  format: { with: VALID_PASSWORD_REGEX }
 
 #validates email
-  validates :email,
-    presence: true,
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-
-#validates id
-  validates :id,
-    presence: true,
-    uniqueness: true
+validates :email,
+  presence: true,
+  length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
 
 #validates names
-  validates :firstname, :lastname,
-    presence:true
+validates :firstname, :lastname,
+  presence:true
 
 #validates phone number field
-  validates :phone,
-    presence: true,
-    format: { with: VALID_PHONE_NUMBER_REGEX }
-
-#validates addresses
-  # validates :address,
-  #   presence: true
+validates :phone,
+  presence: true,
+  format: { with: VALID_PHONE_NUMBER_REGEX }
 
 #validates license_plate
 #note: change regex for all 50 states once Loopie expands, will use regex for washington LPs for now
-  validates :license_plate,
-    presence: true,
-    format: { with: VALID_LP_REGEX }
-
-
+validates :license_plate,
+  presence: true,
+  format: { with: VALID_LP_REGEX }
 
 #validates car_year
-  validates :car_year,
-    presence: true,
-    #only_integer: true,
-    allow_nil: false
-    #in: 1920..2019
-
-
-
-
-#validates car_make
-
-#validates car_model
-
-#validates car_color
-
-#validates VIN(vehicle identification number)
-
-#validates driver_license_img
-
-#validates description
-
-#validates current_loads
-
-#validates start_date
-
-#validates driving_status
-
-#validates confirmation_status
-
-#validates created_at
-
-#validates updated_at
+validates :car_year,
+  presence: true,
+  #only_integer: true,
+  allow_nil: false
+  #in: 1920..2019
 
 
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -7,7 +7,6 @@ class DriverTest < ActiveSupport::TestCase
       username: "ExUsername",
       password: "ExPassword!23",
       email:"yeet@yeet.com",
-      id:"230",
       phone:"(123)-456-789",
       firstname:"John",
       lastname:"Lee",
@@ -15,7 +14,6 @@ class DriverTest < ActiveSupport::TestCase
       car_year: "2000"
     )
   end
-
 
   test "Username should be present" do
     @Driver.username = ""
@@ -43,28 +41,22 @@ class DriverTest < ActiveSupport::TestCase
     assert_not @Driver.valid?
   end
 
-#test fails for some stupid reason, regex works fine, i have no idea why
-#because it works in cmd but not in rails model
+  #test fails for some stupid reason, regex works fine, i have no idea why
+  #because it works in cmd but not in rails model
 
-  # test "license_plate should accept valid license plates" do
-  #   valid_license_plates = %w[273-FSD ASD2384 837-AHX OFC3827]
-  #   valid_license_plates.each do |valid_lp|
-  #     @Driver.license_plate = valid_lp
-  #     assert @Driver.valid?, "#{valid_lp.inspect} should be valid"
-  #   end
-  # end
+   #test "license_plate should accept valid license plates" do
+    # valid_license_plates = %w[273-FSD ASD2384 837-AHX OFC3827]
+     #valid_license_plates.each do |valid_lp|
+      # @Driver.license_plate = valid_lp
+       #assert @Driver.valid?, "#{valid_lp.inspect} should be valid"
+     #end
+   #end
 
-  # test "car_year should only accept valid years" do
-  #   valid_car_years = %w[2000 2018 1966 1940]
-  #   valid_car_years.each do |valid_car_year|
-  #     @Driver.car_year = valid_car_year
-  #     assert @Driver.valid?, "#{valid_car_year} should be valid"
-  #   end
-  # end
-
-
-
-  # test "the truth" do
-  #   assert true
-  # end
+   #test "car_year should only accept valid years" do
+    # valid_car_years = %w[2000 2018 1966 1940]
+     #valid_car_years.each do |valid_car_year|
+      # @Driver.car_year = valid_car_year
+       #assert @Driver.valid?, "#{valid_car_year} should be valid"
+    # end
+   #end
 end
