482aa48ee7f5423faa68c7f2377571511fd136aa
johncburns1 <johncburns1@gmail.com>
2018-10-02 13:36:52 -0700

    fix available_zip_codes migration file

--- db/migrate/20181002201412_available_zip_codes_table.rb
+++ db/migrate/20181002201412_available_zip_codes_table.rb
@@ -1,7 +1,7 @@
 class CreateLoads < ActiveRecord::Migration[5.2]
   def change
     create_table :available_zip_codes do |t|
-      t.integer :zip_code
+      t.string :zip_code
       t.timestamps
     end
   end
