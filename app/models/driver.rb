ea5b78e4fb3ae24546ac08bda9d6399dca0c500c
johncburns1 <johncburns1@gmail.com>
2018-10-02 15:33:33 -0700

    add aws credentials for active storage

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,6 +4,7 @@ class Driver < ApplicationRecord
   before_save { self.confirmation_status = 1 }
   before_save { self.driving_status = 0 }
   has_many :loads
+  #has_one_attached :license_image
 
     # def VINVALIDATE
     #   unless vin.include?('I', 'Q', 'O') == false
