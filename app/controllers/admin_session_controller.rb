fde2eadf96f2110fca9329fcf07ffd3b3723d758
loopiellcdev <loopiellcdev@gmail.com>
2018-10-09 14:15:33 -0700

    added more signin stuff

--- app/controllers/admin_session_controller.rb
+++ app/controllers/admin_session_controller.rb
@@ -15,6 +15,8 @@ class AdminSessionController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
--- app/helpers/admin_session_helper.rb
+++ app/helpers/admin_session_helper.rb
@@ -8,4 +8,14 @@ module AdminSessionHelper
       @current_admin ||= Admin.find_by(id: session[:admin_id])
     end
   end
+
+  def log_out
+   session.delete(:admin_id)
+   @current_admin = nil
+ end
+
+ def logged_in?
+   !current_user.nil?
+ end
+
 end
