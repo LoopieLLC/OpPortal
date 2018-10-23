fdeb32bf357a8064f7ecffc449965daf2c259cd3
johncburns1 <johncburns1@gmail.com>
2018-10-23 16:28:04 -0700

    syntax

--- app/helpers/admin_session_helper.rb
+++ app/helpers/admin_session_helper.rb
@@ -9,16 +9,16 @@ module AdminSessionHelper
     end
   end
 
- def log_out
-   session.delete(:admin_id)
-   @current_admin = nil
- end
+  def log_out
+    session.delete(:admin_id)
+    @current_admin = nil
+  end
 
- def logged_in?
-   !current_user.nil?
- end
+  def logged_in?
+    !current_user.nil?
+  end
 
- def confirm_user
- end
+  def confirm_user
+  end
 
 end
