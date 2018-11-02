f7e648db3ea7cd87b8221ed34ea7276fcc8cd5e1
johncburns1 <johncburns1@gmail.com>
2018-11-01 18:36:15 -0700

    get login working with sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,6 @@ Rails.application.routes.draw do
   resources :users
   resources :drivers
   resources :washers
-  resources :admins 
+  resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
